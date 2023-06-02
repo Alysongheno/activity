138707ac2f74bfb25858e571ca5f01fa1afbf986
Alyson Gheno <32337574+Alysongheno@users.noreply.github.com>
2023-06-02 18:33:53 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -7,12 +7,21 @@ operador = input('insira um operador(+ - / *):')
 
 numero2 = float(input('insira o segundo número:'))
 
-for valor in operador:
-    if valor == '+':
-        print('O resultado é:', numero1 + numero2)
-    if valor == '-':
-        print('O resultado é:', numero1 - numero2)
-    if valor == '*':
-        print('O resultado é:', numero1 * numero2)
-    if valor == '/':
-        print('O resultado é:', numero1 / numero2)
+if operador == '+':
+    resultado = numero1 + numero2
+    print('O resultado é:', resultado)
+    
+elif operador == '-':
+    resultado = numero1 - numero2
+    print('O resultado é:', resultado)    
+
+elif operador == '*':
+    resultado = numero1 * numero2
+    print('O resultado é:', resultado)    
+
+elif operador == '/':
+    resultado = numero1 / numero2
+    print('O resultado é:', resultado)
+    
+else:
+    print('Operação inválida!')
