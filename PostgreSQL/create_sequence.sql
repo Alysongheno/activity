249d8b20358b5549db3627996846ffe2aefc8119
Alyson Gheno <32337574+Alysongheno@users.noreply.github.com>
2023-10-19 19:39:24 -0300

    Update create_sequence.sql

--- PostgreSQL/create_sequence.sql
+++ PostgreSQL/create_sequence.sql
@@ -1,10 +1,8 @@
 --Criando sequence para tabela sem campo SERIAL
-
 CREATE TEMPORARY SEQUENCE auto_sequence
     INCREMENT BY 1
 	MINVALUE 1
     START WITH 1;
---DROP sequence auto_sequence;
 
 --serial atual
 SELECT CURRVAL ('auto_sequence');
@@ -16,7 +14,6 @@ CREATE TEMPORARY TABLE auto (
         id int PRIMARY KEY DEFAULT NEXTVAL ('auto_sequence'),
         nome VARCHAR(30) NOT NULL
 );
---DROP TABLE if exists auto;
 
 --Inserindo valores 
 INSERT INTO auto (nome) VALUES ('Arthur Gheno');
@@ -27,4 +24,3 @@ INSERT INTO auto (id, nome) VALUES (2, 'Arthur Gheno');
 INSERT INTO auto (nome) VALUES ('Gabriele dos Santos b');
 
 SELECT * FROM auto;
-
